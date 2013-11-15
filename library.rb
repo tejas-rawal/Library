e3e83b7d7f5b369276ac6d682ed4dfb0caa36710
Tejas Rawal <trawal90@gmail.com>
2013-11-15 11:46:36 -0600

    Add add_book, borrowed_book, and list_books methods

--- library.rb
+++ library.rb
@@ -1,48 +1,73 @@
 class Library
+
   def initialize
     @books = []
   end
 
-  def books
-  end
+  # def books
+  #   puts @books.join(", ")
+  # end
 
   def list_books
+    @books.each do |book|
+      library_hash = {}
+      library_hash[book] = book.status
+      puts library_hash
+    end
   end
 
   def borrowed_books
+  @books.each do |book|
+    if book.status != "available"
+      book.status = "borrowed"
+    end
   end
 
-  def available_books
-  end
+  # def available_books
+  # end
 
   def add_book(book)
+    @books.push(book)
   end
 
-  def check_out(user, book)
-  end
+  # def check_out(user, book)
+  #   @user = user
 
-  def check_in(book)
-  end
+
+  # end
+
+#   def check_in(book)
+#   end
 end
 
-class Borrower
-  def initialize(name)
-  end
+# class Borrower
+#   attr_writer :borrowed_books
+#   def initialize(name)
+#     @name = name
+#   end
 
-  def borrowed_books
-  end
+#   def borrowed_books(borrower, book)
+#     @borrowed_books = {} 
+#   end
 
-  def name
-  end
+#   def name
+#   end
 
-  def borrowed_books_count
-  end
+#   def borrowed_books_count
+#   end
 
-  def borrowed_books_list
-  end
-end
+#   def borrowed_books_list
+#   end
+# end
 
 class Book
+  attr_reader :title, :author
+  attr_successor :status, :borrower
+
   def initialize(title, author)
+    @title = title
+    @author = author
+    @status = "available"
+    @borrower = nil
   end
 end
