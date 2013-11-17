235b69e59f23ebd36c3189eb70e2344073fcb320
Tejas Rawal <trawal90@gmail.com>
2013-11-17 04:51:08 -0600

    Working library code

--- library.rb
+++ library.rb
@@ -1,73 +1,142 @@
 class Library
 
   def initialize
+    
     @books = []
+  
   end
 
-  # def books
-  #   puts @books.join(", ")
-  # end
+  def books
+    
+    puts @books.join(", ")
+  
+  end
 
   def list_books
+    
+    listed_books = {}
+
     @books.each do |book|
-      library_hash = {}
-      library_hash[book] = book.status
-      puts library_hash
+      listed_books[book.title] = book.status 
+
     end
+
   end
 
   def borrowed_books
-  @books.each do |book|
-    if book.status != "available"
-      book.status = "borrowed"
+    
+    borrowed_books = {}
+    
+    @books.each do |book|
+      if book.status == :borrowed
+        
+        borrowed_books[book] = book.borrower
+
+      end
+
     end
+  
   end
 
-  # def available_books
-  # end
+  def available_books
+    
+    available_books = []
+    
+    @books.each do |book|
+      
+      if book.status == :available
+        
+        available_books << book.title
+
+      end
+
+    end
+
+  end
 
   def add_book(book)
-    @books.push(book)
+    
+    @books << book
+  
   end
 
-  # def check_out(user, book)
-  #   @user = user
+  def check_out(user, book)
+    
+    if book.status == :available
+      
+      if user.user_books.size < 2
+        
+        book.borrower = user
+        user.user_books << book
+        book.status = :borrowed
+      
+      else
+        
+        puts "Sorry, you cannot check out any more books"
+      
+      end
+    
+    else
+      
+      puts "Sorry, that book is not available"
+    
+    end
+  
+  end
 
+  def check_in(book)
+    
+    @books << book
+    book.status = :available
+    book.borrower = nil
+    user_books.delete(book)
+  
+  end
 
-  # end
-
-#   def check_in(book)
-#   end
 end
 
-# class Borrower
-#   attr_writer :borrowed_books
-#   def initialize(name)
-#     @name = name
-#   end
+class Borrower
+  
+  attr_accessor :user_books
+  
+  def initialize(name)
+    
+    @name = name
+    @user_books = []
+  
+  end
 
-#   def borrowed_books(borrower, book)
-#     @borrowed_books = {} 
-#   end
+  def borrowed_books
+  
+  end
 
-#   def name
-#   end
 
-#   def borrowed_books_count
-#   end
+  def borrowed_books_count
+  
+  end
 
-#   def borrowed_books_list
-#   end
-# end
+  def borrowed_books_list
+    
+    @user_books
+    
+    puts "#{@user_books}"
+
+  end
+
+end
 
 class Book
+  
   attr_reader :title, :author
-  attr_successor :status, :borrower
+  attr_accessor :status, :borrower
 
   def initialize(title, author)
+    
     @title = title
     @author = author
-    @status = "available"
+    @status = :available
     @borrower = nil
+  
   end
+
 end
