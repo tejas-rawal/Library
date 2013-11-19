bd4be606d2dba02a0c26b5ee900e6fc6e47cb152
Tejas Rawal <trawal90@gmail.com>
2013-11-18 19:02:09 -0600

    Complete project with comments

--- library.rb
+++ library.rb
@@ -1,16 +1,28 @@
 class Library
 
+  #initialize books array of class Library
+
   def initialize
     
     @books = []
   
   end
 
+# joins all books in library into a string
+
+# seperated by a ", "
+
+# Example: "Book1, Book2, Book3, ..."
+
   def books
     
     puts @books.join(", ")
   
   end
+
+# Creates a hash with keys = the title of the books in the Library
+
+# and the values = their status (either available or borrowed)
 
   def list_books
     
@@ -23,6 +35,10 @@ class Library
 
   end
 
+# Creates a hash contaiting the borrowed books
+
+# with the keys = book titles and the values = the name of the borrower
+
   def borrowed_books
     
     borrowed_books = {}
@@ -30,13 +46,17 @@ class Library
     @books.each do |book|
       if book.status == :borrowed
         
-        borrowed_books[book] = book.borrower
+        borrowed_books[book.title] = book.borrower
 
       end
 
     end
   
   end
+
+# Creates an array containing all book that are available in the library
+
+# Checks to see if book is available before pushing to array
 
   def available_books
     
@@ -54,11 +74,17 @@ class Library
 
   end
 
+# Adds book to the @books array
+
   def add_book(book)
     
     @books << book
   
   end
+
+# Check out a book that is available to a borrower
+
+# Borrower can only check out book if they have less than 2
 
   def check_out(user, book)
     
@@ -84,6 +110,12 @@ class Library
   
   end
 
+# Check in a book that has been borrowed
+
+# Push the book back into the books array
+
+# Deletes the book from the borrower's array
+
   def check_in(book)
     
     @books << book
@@ -96,6 +128,8 @@ class Library
 end
 
 class Borrower
+
+  #initialize attributes of the Borrower
   
   attr_accessor :user_books
   
@@ -115,6 +149,10 @@ class Borrower
   
   end
 
+# Returns @user_books array which includes all books the Borrower
+
+# has checked out
+
   def borrowed_books_list
     
     @user_books
@@ -127,6 +165,8 @@ end
 
 class Book
   
+  # initialize attributes of the Book class
+
   attr_reader :title, :author
   attr_accessor :status, :borrower
 
